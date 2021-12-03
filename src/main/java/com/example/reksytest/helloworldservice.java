b82c53522f4a3416a437a64c4bca94230f617914
Reksy <petrus.chanel@pegipegi.com>
2021-12-03 11:21:29 +0700

    versi1

---
+++ src/main/java/com/example/reksytest/helloworldservice.java
@@ -0,0 +1,13 @@
+package com.example.reksytest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloworldservice {
+
+    @RequestMapping("/")
+    public String index(){
+        return ("Halo Dunia 2022 server");
+    }
+}
---
+++ src/test/java/com/example/reksytest/ReksyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reksytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReksyTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
